5f74cbfc70756a1b9a817c2adff25c218ad1adff
Silas Schnurr <silas@schnurr.one>
2022-04-12 10:56:51 +0200

    init

---
+++ src/main/java/com/webservice/springerstiefel/category/CategoryController.java
@@ -0,0 +1,10 @@
+package com.webservice.springerstiefel.category;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/category")
+public class CategoryController {
+
+}
