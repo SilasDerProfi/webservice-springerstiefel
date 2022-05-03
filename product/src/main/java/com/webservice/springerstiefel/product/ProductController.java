26a4aec9296de65b0f86db3e52956efb17ce0d2b
Silas Schnurr <silas@schnurr.one>
2022-05-03 10:21:53 +0200

    Check Category when creating product

--- product/src/main/java/com/webservice/springerstiefel/product/ProductController.java
+++ product/src/main/java/com/webservice/springerstiefel/product/ProductController.java
@@ -1,9 +1,12 @@
 package com.webservice.springerstiefel.product;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.Iterator;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping(path = "/product")
@@ -12,24 +15,30 @@ public class ProductController {
     private ProductRepository prodRepo;
 
     @PostMapping(path = "/addProd")
-    public @ResponseBody
-    String addNewProduct(@RequestParam String name, @RequestParam int categoryId, @RequestParam double price) {
-        //TODO: check if category exists before adding product
+    public @ResponseBody Product addNewProduct(@RequestParam String name, @RequestParam int categoryId,
+            @RequestParam double price) {
+        RestTemplate restTemplate = new RestTemplate();
+        String prodResourceUrl = "http://productservice:8082/category/categoryExists?productId=" + categoryId;
+        ResponseEntity<Boolean> result = restTemplate.exchange(prodResourceUrl, HttpMethod.GET, null, Boolean.class);
+
+        if (!result.getBody())
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND,
+                    "category with id " + categoryId + " does not exist");
+
         Product p = new Product(name, categoryId, price);
-        prodRepo.save(p);
-        //TODO: change return type?
-        return "success";
+        p = prodRepo.save(p);
+        return p;
     }
 
-    //TODO: Change mapping to delete?
+    // TODO: Change mapping to delete?
     @DeleteMapping(path = "/deleteProd")
     public @ResponseBody String deleteProduct(@RequestParam int id) {
         prodRepo.deleteById(id);
-        //TODO: change return type?
+        // TODO: change return type?
         return "success";
     }
 
-    //TODO: change mapping to delete?
+    // TODO: change mapping to delete?
     @DeleteMapping(path = "/deleteProdByCat")
     public @ResponseBody String deleteProductByCatId(@RequestParam int categoryId) {
         Iterable<Product> products = prodRepo.findAll();
@@ -38,11 +47,11 @@ public class ProductController {
                 prodRepo.delete(p);
             }
         }
-        //TODO: change return type?
+        // TODO: change return type?
         return "success";
     }
 
-    @GetMapping(path="/allProds")
+    @GetMapping(path = "/allProds")
     public @ResponseBody Iterable<Product> getAll() {
         return prodRepo.findAll();
     }
