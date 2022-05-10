916ff09e5cdd329142a3e6feee96fca12b9a7778
Felix <48395059+Felixleh@users.noreply.github.com>
2022-05-10 12:10:27 +0200

    continue

--- product/src/main/java/com/webservice/springerstiefel/product/ProductController.java
+++ product/src/main/java/com/webservice/springerstiefel/product/ProductController.java
@@ -1,12 +1,21 @@
 package com.webservice.springerstiefel.product;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -18,7 +27,7 @@ public class ProductController {
 
     @PostMapping()
     public @ResponseBody Product addNewProduct(@RequestParam String name, @RequestParam int categoryId,
-            @RequestParam double price) {
+            @RequestParam double price, @RequestParam String details) {
         RestTemplate restTemplate = new RestTemplate();
         String prodResourceUrl = "http://categoryservice:8081/categories/categoryExists/" + categoryId;
         ResponseEntity<Boolean> result = restTemplate.exchange(prodResourceUrl, HttpMethod.GET, null, Boolean.class);
@@ -27,7 +36,7 @@ public class ProductController {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND,
                     "category with id " + categoryId + " does not exist");
 
-        Product p = new Product(name, categoryId, price);
+        Product p = new Product(name, price, categoryId, details);
         p = prodRepo.save(p);
         return p;
     }
@@ -54,8 +63,23 @@ public class ProductController {
     }
 
     @GetMapping()
-    public @ResponseBody Iterable<Product> getAll() {
-        return prodRepo.findAll();
+    public @ResponseBody Iterable<Product> getAll(
+    		@RequestParam(defaultValue="") String description,
+    		@RequestParam(defaultValue="0") int minPrice,
+    		@RequestParam(defaultValue="" + Integer.MAX_VALUE) int maxPrice) {
+    	List<Product> products = new ArrayList<>();
+    	
+    	for (var p : prodRepo.findAll()) {
+    		if (p.getPrice() >= maxPrice)
+    			continue;
+    		if (p.getPrice() < minPrice)
+    			continue;
+    		if (!p.getDetails().contains(description))
+    			continue;
+    		products.add(p);
+    	}
+    	
+        return products;
     }
 
     @GetMapping(path = "/{id}")
