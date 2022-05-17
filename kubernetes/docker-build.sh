122c9e3db80dc61fc97917c1d48143849fee8952
Mika2147 <mika.auer@icloud.com>
2022-05-17 12:24:10 +0200

    updated docker and deployment script

--- kubernetes/docker-build.sh
+++ kubernetes/docker-build.sh
@@ -2,18 +2,19 @@
 if [ -z "$DOCKER_ACCOUNT" ]; then
     DOCKER_ACCOUNT=mika2147
 fi;
-docker build --tag=microservice-kubernetes-apache ../apache
+
+docker build ../apache --tag=microservice-kubernetes-apache
 docker tag microservice-kubernetes-apache $DOCKER_ACCOUNT/microservice-kubernetes-apache:latest
 docker push $DOCKER_ACCOUNT/microservice-kubernetes-apache
 
-docker build . --tag=microservice-kubernetes-category -f ../docker/CategoryDockerfile
+docker build .. --tag=microservice-kubernetes-category -f ../docker/CategoryDockerfile
 docker tag microservice-kubernetes-category $DOCKER_ACCOUNT/microservice-kubernetes-category:latest
 docker push $DOCKER_ACCOUNT/microservice-kubernetes-category
 
-docker build . --tag=microservice-kubernetes-product -f ../docker/ProductDockerfile
+docker build .. --tag=microservice-kubernetes-product -f ../docker/ProductDockerfile
 docker tag microservice-kubernetes-product $DOCKER_ACCOUNT/microservice-kubernetes-product:latest
 docker push $DOCKER_ACCOUNT/microservice-kubernetes-product
 
-docker build . --tag=microservice-kubernetes-database -f ../docker/MySQLDockerfile
+docker build .. --tag=microservice-kubernetes-database -f ../docker/DockerfileMySQL
 docker tag microservice-kubernetes-database $DOCKER_ACCOUNT/microservice-kubernetes-database:latest
 docker push $DOCKER_ACCOUNT/microservice-kubernetes-database
