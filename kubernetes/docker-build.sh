7f8a2023c7b7c7ca6a04e62097e7608b78126499
Mika2147 <mika.auer@icloud.com>
2022-05-18 15:21:40 +0200

    changed docker build script so it can be used with "--no-cache" option, apache reverse proxy works now

--- kubernetes/docker-build.sh
+++ kubernetes/docker-build.sh
@@ -3,15 +3,30 @@ if [ -z "$DOCKER_ACCOUNT" ]; then
     DOCKER_ACCOUNT=mika2147
 fi;
 
-docker build ../apache --tag=microservice-kubernetes-apache
+arg1="$1"
+
+if [ "$1" == "--no-cache" ]; then
+    docker build ../apache --tag=microservice-kubernetes-apache --no-cache
+else
+    docker build ../apache --tag=microservice-kubernetes-apache
+fi
+
 docker tag microservice-kubernetes-apache $DOCKER_ACCOUNT/microservice-kubernetes-apache:latest
 docker push $DOCKER_ACCOUNT/microservice-kubernetes-apache
 
-docker build .. --tag=microservice-kubernetes-category -f ../docker/CategoryDockerfile
+if [ "$1" == "--no-cache" ]; then
+    docker build .. --tag=microservice-kubernetes-category -f ../docker/CategoryDockerfile --no-cache
+else
+    docker build .. --tag=microservice-kubernetes-category -f ../docker/CategoryDockerfile
+fi
 docker tag microservice-kubernetes-category $DOCKER_ACCOUNT/microservice-kubernetes-category:latest
 docker push $DOCKER_ACCOUNT/microservice-kubernetes-category
 
-docker build .. --tag=microservice-kubernetes-product -f ../docker/ProductDockerfile
+if [ "$1" == "--no-cache" ]; then
+    docker build .. --tag=microservice-kubernetes-product -f ../docker/ProductDockerfile --no-cache
+else
+    docker build .. --tag=microservice-kubernetes-category -f ../docker/CategoryDockerfile
+fi
 docker tag microservice-kubernetes-product $DOCKER_ACCOUNT/microservice-kubernetes-product:latest
 docker push $DOCKER_ACCOUNT/microservice-kubernetes-product
 
